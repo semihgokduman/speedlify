d4acd804e16d99874be50a3e1cd72d0d6e27e0e6
Semih Gökduman <semihgokduman@gmail.com>
2020-07-16 16:37:41 +0200

    new configuration

---
+++ _data/sites/kfzteile24.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: "kfzteile24", // optional, falls back to object key
+	description: "kfzteile24 web sites",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		freshChrome: "site",
+	},
+	urls: [
+    "https://www.carpardoo.dk/",
+    "https://www.carpardoo.fr/",
+    "https://www.carpardoo.nl/",
+    "https://www.carpardoo.pl",
+    "https://www.kfzteile24.at/",
+    "https://www.kfzteile.com/",
+	]
+};
